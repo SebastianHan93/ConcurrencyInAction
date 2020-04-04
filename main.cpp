67e7035cce494ecd290b89035405fb8817fc5a3d
SebastianHan93 <null93@hotmail.com>
2020-04-04 12:22:04 +0800

    Add ParallelAccumulate.h

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include "ParallelAccumulate.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    clock_t start, finish;
+    double  duration;
+
+    std::vector<int> num(100000000,5);
+
+    start = clock();
+    int parallelRet =  parallel_accumulate(num.begin(),num.end(), 0);
+    finish = clock();
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+
+    std::cout << "duration:" << duration <<std::endl;
+    std::cout << parallelRet <<std::endl;
+    std::cout << "-------------------------------" <<std::endl;
+
+    std::vector<int> num1(100000000,5);
+    start = clock();
+    int singleRet=std::accumulate(num1.begin(),num1.end(),0);
+    finish = clock();
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    std::cout << "duration:" << duration <<std::endl;
+    std::cout << singleRet <<std::endl;
     return 0;
 }
